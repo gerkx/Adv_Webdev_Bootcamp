02d274a214f36b96d19ecc111304c80cc16229c7
Patrick Gerke <ptgerke@gmail.com>
2018-06-09 16:30:55 +0200

    reduce exercises completed

--- advancedArrayMethods/reduceExercise.js
+++ advancedArrayMethods/reduceExercise.js
@@ -13,7 +13,9 @@ function extractValue(arr, key){
 
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel 
+and the values as the number of times the vowel appears in the string. 
+This function should be case insensitive so a lowercase letter and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -23,12 +25,25 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str){
-   
+function vowelCount(str){    
+    return str.toLowerCase().split("").reduce((acc, next) => {
+        const vowels = "aeiou";
+        if(vowels.indexOf(next) !== -1){
+            if(next in acc){
+                acc[next]++;
+            }else{
+                acc[next] = 1;
+            }
+        }
+        return acc;
+    }, {});
 }
 
+// console.log(vowelCount('Elie'));
+
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it 
+with each object now including the key and value passed to the function.
 
 Examples:
     var arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -43,12 +58,20 @@ Examples:
 */
 
 function addKeyAndValue(arr, key, value){
-    
+    return arr.reduce((acc, next, index) => {
+        arr[index][key] = value;
+        return acc;
+    }, arr);
 }
 
+// var arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
+// console.log(addKeyAndValue(arr, 'title', 'Instructor'));
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. 
+The partition function should run the callback function on each value in the array and if the result of 
+the callback function at that specific value is true, the value should be placed in the first subarray. 
+If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
     
@@ -70,5 +93,19 @@ Examples:
 */
 
 function partition(arr, callback){
-    
+    return arr.reduce((acc, next) => {
+        if(callback(next)){
+            acc[0].push(next);
+        }else{
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[],[]]);
 }
+
+// function isEven(val){
+//     return val % 2 === 0;
+// }
+// var arr = [1,2,3,4,5,6,7,8];
+
+// console.log(partition(arr, isEven));
